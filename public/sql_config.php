9d6ccc20e49a9f07d1c8f5b3a4e4c443e83e1e52
Gabriel Patrial <46763529+ghcostad@users.noreply.github.com>
2020-04-08 23:20:22 -0400

    Update sql_config.php

--- public/sql_config.php
+++ public/sql_config.php
@@ -1,4 +1,4 @@
 <?php
-	$con = mysqli_connect("fdb17.biz.nf", "3346580_ghcostad", "Pepsi2xcola!") or die("Unable to connect.");
+	$con = mysqli_connect("fdb17.biz.nf", "3346580_ghcostad", "Password") or die("Unable to connect.");
 	mysqli_select_db($con, "3346580_ghcostad");
 ?>
